cc919a9cc91845e742f944c3ffc07510477125b8
Mehmet Demirtas <mvdiesel@users.noreply.github.com>
2018-12-20 11:16:00 +0300

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5;
+    public Rigidbody2D rb;
+    public GameObject bullet;
+    public Transform muzzle;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        Rotation();
+        Attack();
+	}
+
+    void Movement()
+    {
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position += Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+    }
+
+    void Rotation()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+    }
+
+    void Attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet = Instantiate(bullet , muzzle.position , transform.rotation);
+           
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public BoxCollider2D bc;
+    public Rigidbody2D rb;
+    public float speed;
+	// Use this for initialization
+	void Awake () {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed * Time.fixedDeltaTime;
+
+    }
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    void Impact()
+    {
+
+    }
+}
